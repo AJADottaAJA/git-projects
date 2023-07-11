cfaf18683b55417bd23857626d310dea7167a904
aliEbrahimian <abtinebrahimlia@gmail.com>
2023-07-11 10:50:10 +0330

    py cpp html changes

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -5,3 +5,9 @@ int main()
     std::cout<<2*5
     b = 45
 }
+
+int jam_zarb(int a, int b)
+{
+    int a[2] = {a+b, a*b}
+    return a
+}
--- python.py
+++ python.py
@@ -4,3 +4,9 @@ print("salam")
 print(type("salam"))
 
 a = int("121")
+
+def f(a, b):
+    return a+b
+
+def os_walk():
+    return os.os_walk()
